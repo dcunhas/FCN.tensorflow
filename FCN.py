54dca44965178b3986269f7d3443ba959a86ebd9
rishab <rjaggi98@gmail.com>
2017-10-26 11:33:15 -0400

    Changed prediction numpy array to a tensor

--- FCN.py
+++ FCN.py
@@ -265,7 +265,7 @@ def main(argv=None):
                                                         keep_probability: 1.0})
             pred = np.squeeze(pred, axis=3)
 
-            prediction_tensor = tf.pack(pred)
+            prediction_tensor = tf.stack(pred)
 
             iou, update_op = tf.metrics.mean_iou(true_label, prediction_tensor, NUM_OF_CLASSESS)
             iou_array.append(iou)
